75a32730286ea900216b9e99b9c95ffae82c0757
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-26 15:31:55 +0800

    chore(server): support certificate file path

--- cmd/server/chi/main.go
+++ cmd/server/chi/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
@@ -16,13 +18,29 @@ import (
 	"golang.org/x/net/http2/h2c"
 )
 
+var (
+	listen   int
+	certPath string
+	keyPath  string
+)
+
 func main() {
+	flag.IntVar(&listen, "l", 8080, "listen port")
+	flag.StringVar(&certPath, "c", "", "cert path")
+	flag.StringVar(&keyPath, "k", "", "key portpath")
+	flag.Parse()
+
+	h := h2c.NewHandler(
+		router.New(),
+		&http2.Server{},
+	)
+	if certPath != "" && keyPath != "" {
+		h = router.New()
+	}
+
 	srv := &http.Server{
-		Addr: ":8080",
-		Handler: h2c.NewHandler(
-			router.New(),
-			&http2.Server{},
-		),
+		Addr:              ":" + strconv.Itoa(listen),
+		Handler:           h,
 		ReadHeaderTimeout: time.Second,
 		ReadTimeout:       5 * time.Minute,
 		WriteTimeout:      5 * time.Minute,
@@ -32,10 +50,11 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		if err := listenAndServe(srv, certPath, keyPath); !errors.Is(err, http.ErrServerClosed) {
 			log.Fatalf("HTTP listen and serve: %v", err)
 		}
 	}()
+	log.Println("server listen on port: " + strconv.Itoa(listen))
 
 	<-signals
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -43,4 +62,12 @@ func main() {
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatalf("HTTP shutdown: %v", err) // nolint:gocritic
 	}
+}
+
+func listenAndServe(s *http.Server, certPath string, keyPath string) error {
+	if certPath != "" && keyPath != "" {
+		return s.ListenAndServeTLS(certPath, keyPath)
+	}
+
+	return s.ListenAndServe()
 }
--- cmd/server/gin/main.go
+++ cmd/server/gin/main.go
@@ -3,33 +3,44 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/go-training/proto-go-sample/cmd/server/gin/router"
+
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
-func grpcHandler(h http.Handler) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		log.Println("protocol version:", c.Request.Proto)
-		h.ServeHTTP(c.Writer, c.Request)
-	}
-}
+var (
+	listen   int
+	certPath string
+	keyPath  string
+)
 
 func main() {
+	flag.IntVar(&listen, "l", 8080, "listen port")
+	flag.StringVar(&certPath, "c", "", "cert path")
+	flag.StringVar(&keyPath, "k", "", "key portpath")
+	flag.Parse()
+
+	h := h2c.NewHandler(
+		router.New(),
+		&http2.Server{},
+	)
+	if certPath != "" && keyPath != "" {
+		h = router.New()
+	}
+
 	srv := &http.Server{
-		Addr: ":8080",
-		Handler: h2c.NewHandler(
-			router.New(),
-			&http2.Server{},
-		),
+		Addr:              ":" + strconv.Itoa(listen),
+		Handler:           h,
 		ReadHeaderTimeout: time.Second,
 		ReadTimeout:       5 * time.Minute,
 		WriteTimeout:      5 * time.Minute,
@@ -39,10 +50,11 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		if err := listenAndServe(srv, certPath, keyPath); !errors.Is(err, http.ErrServerClosed) {
 			log.Fatalf("HTTP listen and serve: %v", err)
 		}
 	}()
+	log.Println("server listen on port: " + strconv.Itoa(listen))
 
 	<-signals
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -50,4 +62,12 @@ func main() {
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatalf("HTTP shutdown: %v", err) // nolint:gocritic
 	}
+}
+
+func listenAndServe(s *http.Server, certPath string, keyPath string) error {
+	if certPath != "" && keyPath != "" {
+		return s.ListenAndServeTLS(certPath, keyPath)
+	}
+
+	return s.ListenAndServe()
 }
